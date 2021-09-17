6bb892f23e46fc59ef9d00bcef0d4fae38a38a75
Jakub Szewczyk <git@kubasz.xyz>
2021-09-17 14:59:38 +0100

    Update util/upload

--- util/upload.sh
+++ util/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for FUNC in get_simple put_simple wordcount wordcount_manual_ndp thumbnailer
+for FUNC in get_simple put_simple wordcount grep sha256sum thumbnailer
 do
     inv func.compile ndp $FUNC
     inv func.upload ndp $FUNC
