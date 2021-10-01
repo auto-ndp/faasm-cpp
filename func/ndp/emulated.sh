c9210bed9c106464d839333a4e26b7b68d544bf9
Jakub Szewczyk <git@kubasz.xyz>
2021-10-01 12:22:38 +0100

    Emulated builds for pintool checking

--- func/ndp/emulated.sh
+++ func/ndp/emulated.sh
@@ -4,3 +4,5 @@ EMUCLANGPP="clang++ -std=c++17 -O2 -g -I. -Iapi -I../../libfaasm/ pintool_emu.cp
 
 ${EMUCLANGPP} json_parse.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o json_parse_emu
 ${EMUCLANGPP} wordcount.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o wordcount_emu
+${EMUCLANGPP} sha256sum.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o sha256sum_emu
+${EMUCLANGPP} grep.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o grep_emu
