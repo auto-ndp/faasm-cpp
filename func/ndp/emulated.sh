1e1131535380406cbaf827ed1867e733e96064f5
Jakub Szewczyk <git@kubasz.xyz>
2021-10-05 11:10:18 +0100

    Add Eigen 3.4 distribution

--- func/ndp/emulated.sh
+++ func/ndp/emulated.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EMUCLANGPP="clang++ -std=c++17 -O2 -g -I. -Iapi -I../../libfaasm/ pintool_emu.cpp"
+EMUCLANGPP="clang++ -std=c++17 -O2 -g -I. -Iapi -I../../libfaasm/ -I../../third-party/eigen-3.4/ -DEIGEN_INITIALIZE_MATRICES_BY_ZERO=1 -DEIGEN_NO_AUTOMATIC_RESIZING=1 -DEIGEN_NO_IO=1 -DEIGEN_DONT_PARALLELIZE=1 -DEIGEN_NO_CUDA=1 pintool_emu.cpp"
 
 ${EMUCLANGPP} thumbnailer_decode.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o thumbnailer_decode_emu
 ${EMUCLANGPP} thumbnailer_shrink.cpp $HOME/phd/code/pin-NearMAP/stublib/stublib.a -o thumbnailer_shrink_emu
