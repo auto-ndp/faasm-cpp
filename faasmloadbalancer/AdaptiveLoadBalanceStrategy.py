dd95b2074e4093e407ed881110af17a07d7ccadb
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-01 23:25:13 +0000

    only sends load requests every 10 seconds to prevent overhead

--- faasmloadbalancer/AdaptiveLoadBalanceStrategy.py
+++ faasmloadbalancer/AdaptiveLoadBalanceStrategy.py
@@ -1,34 +1,33 @@
 
 from faasmloadbalancer.ILoadBalanceStrategy import ILoadBalanceStrategy
 import requests
+import time
+
 
 class LeastLoadStrategy(ILoadBalanceStrategy):
     def __init__(self, workers):
         self.workers = workers
+        self.loads = { worker: 0 for worker in workers }
+        self.last_load_time = time.time()
         
-    def get_next_host(self, task_id) -> str:
-        # Get server loads and sort them
-        loads = []
-        for worker in self.workers:
-            
-            # Send request to worker
-            url = "http://{}:8080/f/".format(worker)
-            data = {
-                "function": "get_load",
-                "user": "admin",
-            }
-            
-            data["is_loadrequest"] = True
-            
-            headers =  { "Content-Type" : "application/json" }
-            response = requests.post(url, json=data, headers=headers)
-            
         
-            # Add to list
-            loads.append(float(worker, response.text))
-            
-        # Sort by load
-        loads.sort(key=lambda tup: tup[1])
-        
-        # Return the worker ID
+    def get_next_host(self, task_id) -> str:        
+        if time.time() - self.last_load_time > 10:
+            self.last_load_time = time.time()
+            for worker in self.workers:
+                # Send request to worker
+                url = "http://{}:8080/f/".format(worker)
+                data = {
+                    "function": "get_load",
+                    "user": "admin",
+                }
+                
+                data["load_request"] = True
+                
+                headers =  { "Content-Type" : "application/json" }
+                response = requests.post(url, json=data, headers=headers)
+                
+                self.loads[worker] = float(response.text)
+
+        loads = sorted(self.loads.items(), key=lambda x: x[1])        
         return loads[0][0]
