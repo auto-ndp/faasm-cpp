ad491f188eca31b4e35995ded39e5eb901a88709
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-11 17:28:37 +0000

    Added optional tag to params in load balance strategy

--- faasmloadbalancer/ILoadBalanceStrategy.py
+++ faasmloadbalancer/ILoadBalanceStrategy.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 class ILoadBalanceStrategy(ABC):
     
     @abstractmethod
-    def get_next_host(self, user, func_name):
+    def get_next_host(self, user=None, func_name=None):
         pass
     
     
