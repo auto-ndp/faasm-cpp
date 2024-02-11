60f00087dde9e13ba5a8f3a93a24029b8ec78e0a
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-11 16:07:34 +0000

    removed imports

--- experiments/run_wordcount.py
+++ experiments/run_wordcount.py
@@ -1,14 +1,8 @@
 import time
-from tasks.func import invoke
-from tasks.func import compile
-from tasks.func import upload
-
 import matplotlib.pyplot as plt
 
 
 def run_benchmark(gutenberg_title, user, func, num_runs=10):
-    # Compile the function
-    compile(None, user, func)
 
     # Get the text
     with open("./text_sources/{}.txt".format(gutenberg_title), "r") as f:
