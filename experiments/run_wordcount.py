83844eee364daa4e24d791218abbcfdb4719d41c
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-11 16:26:10 +0000

    escapes single quotations in strings

--- experiments/run_wordcount.py
+++ experiments/run_wordcount.py
@@ -11,14 +11,14 @@ def run_benchmark(gutenberg_title, user, func, num_runs=10):
         start = time.time()
         print("Calling inv func.invoke to upload the text to the user's storage")
         print("Key: ", gutenberg_title)
-        cmd = "inv func.invoke " + user + " put " + "'" + gutenberg_title + " " + text + "'"
+        cmd = "inv func.invoke " + user + " put " + "\'" + gutenberg_title + " " + text + "\'"
         os.system(cmd)
         print("Time taken to upload the text to the user's storage: ", time.time() - start)
         end = time.time()
         
         
         print("Calling inv func.invoke to run the wordcount on the text")
-        cmd = "inv func.invoke " + user + " " + func + " '" + gutenberg_title + "'"
+        cmd = "inv func.invoke " + user + " " + func + " \'" + gutenberg_title + "\'"
         print("Command: ", cmd)
         wordcount_time = time.time()
         os.system(cmd)
