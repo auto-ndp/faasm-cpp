10b3a3f7fc331358c81aa92a453e389bfc9ac287
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-11 16:15:17 +0000

    attempts to add txt file to storage

--- experiments/run_wordcount.py
+++ experiments/run_wordcount.py
@@ -1,12 +1,18 @@
 import time
-
+import os
 
 def run_benchmark(gutenberg_title, user, func, num_runs=10):
 
     # Get the text
     with open("./text_sources/{}.txt".format(gutenberg_title), "r") as f:
         text = f.read()
-        print("Read {} bytes from {}".format(len(text), gutenberg_title))
+        
+        # Upload the txt to the user's storage
+        start = time.time()
+        cmd = "inv func.invoke " + user + " put " + "'" + gutenberg_title + " " + text + "'"
+        print(cmd)
+        os.system(cmd)
+        end = time.time()
         
 if __name__ == "__main__":
     # Run the benchmark
