8cf41b343cd7a02d8aa9fba31ba2860d75884b96
Donald Jennings <D.D.Jennings@outlook.com>
2024-02-13 18:31:59 +0000

    added lock to rr

--- faasmloadbalancer/RoundRobinLoadBalancer.py
+++ faasmloadbalancer/RoundRobinLoadBalancer.py
@@ -1,10 +1,11 @@
 import itertools
 from faasmloadbalancer.ILoadBalanceStrategy import ILoadBalanceStrategy
-
+import threading
 class RoundRobinLoadBalancerStrategy(ILoadBalanceStrategy):
     def __init__(self, workers):
         self.workers = workers
         self.worker_iterator = itertools.cycle(workers)
         
     def get_next_host(self, user=None, func_name=None):
-        return next(self.worker_iterator)
+        with threading.Lock():
+            return next(self.worker_iterator)
